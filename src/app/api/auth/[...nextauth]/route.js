7395282c6552de4d4f91044a696c033943d30a63
Sushmayana Mishra <sushmayanamishra@gmail.com>
2025-05-25 15:48:15 +0530

    Updated Auth

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 // import { authConfig } from "@/lib/authConfig"; // optional, for cleaner config
 
